b11d76f996e4933175288f3338dd6d667e2685be
Robert Karapetyan <Rob997@hotmail.com>
2019-03-26 19:40:22 +0400

    19.40

--- Hackerrank/Hackerrank.cpp
+++ Hackerrank/Hackerrank.cpp
@@ -1,48 +1,77 @@
+#include "pch.h"
 #include <iostream>
 #include <sstream>
+#include <fstream>
+#include <assert.h>
 #include <stack>
 
-int main() {
 
-	std::stack<int> container, outp;
-	std::string ss, maximums;
-	int N;
+int main()
+{
 
-	std::cin >> N;
-	for (int i = 0; i <= N; ++i) {
-		std::getline(std::cin, ss);
-		std::stringstream aa(ss);
+	std::stack<int> maximums, outp;
+
+	int N, crnt;
+	std::ifstream find("text.txt");
+
+	find >> N;
+	for (int i = 0; i < N; ++i) {
+		
+		
 		int max = 0;
-		ss = "";
-		aa >> ss;
-		if (ss == "1") {
-			aa >> ss;
-			if (!outp.empty()) {
-				if (stoi(ss) > outp.top()) {
-					outp.push(stoi(ss));
+		
+		find >> crnt;
+		switch (crnt)
+		{
+		case 1:
+		{
+			find >> crnt;
+			if (!outp.empty())
+			{
+				if (crnt > maximums.top())
+					max = crnt;
+				else
+					max = maximums.top();
+			}
+			else
+			{
+				max = crnt;
+			}
+			outp.push(crnt);
+			if (!maximums.empty())
+			{
+				if (maximums.top() != max)
+				{
+					maximums.push(max);
 				}
-
 			}
-			else {
-				outp.push(stoi(ss));
+			else
+			{
+				maximums.push(max);
 			}
-			container.push(stoi(ss));
+			break;
 		}
-		else if (ss == "2") {
-			if (!container.empty()) {
-				if ((container.top() == outp.top()) && !outp.empty()) {
-					outp.pop();
-					container.pop();
+		case 2:
+		{
+			if (!outp.empty())
+			{
+				if (outp.top() == maximums.top())
+				{
+					maximums.pop();
 				}
-				else {
-					container.pop();
-				}
+				outp.pop();
 			}
+			break;
 		}
-		else if (ss == "3") {
-			std::cout << outp.top() << std::endl;
+		case 3:
+		{
+			std::cout << maximums.top() << std::endl;
 			// if(outp.top() != max)
 			// outp.push(max);
+			break;
+		}
+		default:
+			continue;
 		}
 	}
 	return 0;
