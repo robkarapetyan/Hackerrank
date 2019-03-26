f7d22b810d372604f6a4d3f0b43554b81d5dd4ba
Robert Karapetyan <Rob997@hotmail.com>
2019-03-26 23:55:11 +0400

    completed_maximum_element

--- Hackerrank/Hackerrank.cpp
+++ Hackerrank/Hackerrank.cpp
@@ -6,65 +6,56 @@
 #include <stack>
 
 
-int main()
-{
-	std::stack<int> maximums, outp;
-	int N, crnt;
-	std::ifstream find("text.txt");
-	find >> N;
-	for (int i = 0; i < N; ++i) {
-		int max = 0;
+int main(){
+std::stack<int> maximums, outp;
+int N, crnt;
+std::ifstream find("text.txt");
+find >> N;
+for (int i = 0; i < N; ++i) {
+	int max = 0;
+	find >> crnt;
+	switch (crnt)
+	{
+	case 1:
+	{
 		find >> crnt;
-		switch (crnt)
+		outp.push(crnt);
+		if (!maximums.empty())
 		{
-		case 1:
-		{
-			find >> crnt;
-			if (!outp.empty())
+			if (maximums.top() <= crnt)
 			{
-				if (crnt > maximums.top())
-					max = crnt;
-				else
-					max = maximums.top();
+				maximums.push(crnt);
 			}
-			else
-			{
-				max = crnt;
-			}
-			outp.push(crnt);
-			if (!maximums.empty())
-			{
-				if (maximums.top() != max)
-				{
-					maximums.push(max);
-				}
-			}
-			else
-			{
-				maximums.push(max);
-			}
-			break;
 		}
-		case 2:
+		else
 		{
-			if (!outp.empty())
-			{
-				if (outp.top() == maximums.top())
-				{
-					maximums.pop();
-				}
-				outp.pop();
-			}
-			break;
+			maximums.push(crnt);
 		}
-		case 3:
+		break;
+	}
+	case 2:
+	{
+		if (!outp.empty())
+		{
+			if (outp.top() == maximums.top())
+			{
+				maximums.pop();
+			}
+			outp.pop();
+		}
+		break;
+	}
+	case 3:
+	{
+		if (!maximums.empty())
 		{
 			std::cout << maximums.top() << std::endl;
-			break;
 		}
-		default:
-			continue;
-		}
+		break;
 	}
+	default:
+		continue;
+	}
+}
 	return 0;
 }
